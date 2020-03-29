8bf6101a8fad34e501ae1ece33cbbafce66a018b
Paul Aubry <paulaubry@macbook-pro-de-paul.home>
2020-03-29 01:22:17 +0100

    Better view

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let angle = 0;
-let w = 30;
+let w = 24;
 let ma;
 let maxDist;
 
@@ -22,9 +22,9 @@ function draw() {
     for(let x = 0; x < width; x += w){
       push();
       let d = dist(x,z,width/2,height/2);
-      let offset = map(d, 0, maxDist, -2, 2);
+      let offset = map(d, 0, maxDist, -PI, PI);
       let a = angle + offset;
-      let h = floor(map(sin(a),-1,1,50,400));
+      let h = floor(map(sin(a),-1,1,100,300));
       translate(x - width / 2,0, z - height / 2);
       normalMaterial();
       box(w - 2, h, w - 2);
@@ -32,5 +32,5 @@ function draw() {
       pop();
     }
   }
-  angle += 0.1;
+  angle -= 0.15;
 }
